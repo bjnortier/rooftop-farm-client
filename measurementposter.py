eaee199f14c503574cf6c108c1d47206591e22ce
Ben Nortier <bjnortier@gmail.com>
2016-02-03 17:18:59 +0200

    POST to server

--- measurementposter.py
+++ measurementposter.py
@@ -5,14 +5,25 @@ import sys
 import time
 import sqlite3 as lite
 import json
+import requests
 
 try:
     con = lite.connect('measurements.db')
     cur = con.cursor()
 
     while True:
-        cur.execute('SELECT * FROM measurements LIMIT 50')
+        cur.execute('SELECT id, type, timestamp, data FROM measurements LIMIT 50')
         rows = cur.fetchall()
+
+        payload = list(map(lambda x: {
+            'type': x[1],
+            'timestamp': x[2],
+            'data': x[3],
+        }, rows))
+        print(payload)
+        r = requests.post('http://localhost:3000/api/measurements', json=payload)
+        print(r)
+
         ids = list(map(lambda x: str(x[0]), rows))
         print(ids)
         cur.execute('DELETE FROM measurements WHERE id in (' + ','.join(ids) + ')');
